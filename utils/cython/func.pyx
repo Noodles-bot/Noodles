9a89e959a2adc78d1314ef4d1da9394a5e397571
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-03 02:18:25 +0200

    Fixes

--- utils/cython/func.pyx
+++ utils/cython/func.pyx
@@ -1,3 +1,6 @@
+import codecs
+import os
+import pathlib
 from datetime import datetime
 
 def actions(str action_list):
@@ -9,4 +12,22 @@ def actions(str action_list):
             break
     return actions
 
-def
+def info():
+    cdef list python_files = []
+    cdef list cpp_files = []
+    cdef int total = 0
+    for path, subdirs, files in os.walk('.'):
+        for name in files:
+            if name.endswith('.py'):
+                python_files.append(name)
+            if name.endswith('.cpp'):
+                cpp_files.append(name)
+            try:
+                with codecs.open('./' + str(pathlib.PurePath(path, name)),
+                                 'r', 'utf-8') as f:
+                    for i, l in enumerate(f):
+                        if len(l.strip()) != 0 and name.endswith('.py') or name.endswith('.c++') or name.endswith(
+                                '.c'):
+                            total += 1
+            except:
+                raise SystemError()
