d2deb9733753264ea0be77b0cdce7f6b121dd6b2
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-04 03:52:40 +0200

    test

--- utils/secret.py
+++ utils/secret.py
@@ -5,4 +5,5 @@ password = 'Jupiter22'
 user_agent = 'Noodles'
 TOKEN = 'Njc1NTQyMDExNDU3MDQ0NTEy.XqS67Q.QX7_90kPXFazDXaDIAZzHiULOIw'
 DATABASE = "postgres://zsqtcfcp:ePAhyuUmXR8c7GEwP-Xxjv2xYoaHgrKE@drona.db.elephantsql.com:5432/zsqtcfcp"
+
 repo = 'https://DankDumpster:Jupiter22yolO@github.com/DankDumpster/Noodles'
