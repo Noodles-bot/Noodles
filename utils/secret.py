d50d36058f2b3358dce3e9ff32e68355f802465e
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-04 03:54:32 +0200

    Fixes

--- utils/secret.py
+++
@@ -1,9 +0,0 @@
-client_id = 'mRZ23SHjLS7vFQ'
-client_secret = '11BONHjapuyVA7AvvkeBHmsIV2I'
-username = 'Warrant3112'
-password = 'Jupiter22'
-user_agent = 'Noodles'
-TOKEN = 'Njc1NTQyMDExNDU3MDQ0NTEy.XqS67Q.QX7_90kPXFazDXaDIAZzHiULOIw'
-DATABASE = "postgres://zsqtcfcp:ePAhyuUmXR8c7GEwP-Xxjv2xYoaHgrKE@drona.db.elephantsql.com:5432/zsqtcfcp"
-
-repo = 'https://DankDumpster:Jupiter22yolO@github.com/DankDumpster/Noodles'
