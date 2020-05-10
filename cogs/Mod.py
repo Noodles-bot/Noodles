d8e5693dc5659fa919e951cab15c0b2b3e5c0dea
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-10 18:13:23 +0200

    Fixes

--- cogs/Mod.py
+++ cogs/Mod.py
@@ -10,7 +10,7 @@ class Mod(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.is_owner()
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason="No reason"):
         await member.kick(reason=reason)
         await ctx.send(f"{member.mention} was kicked by {ctx.author.mention}. [{reason}]")
