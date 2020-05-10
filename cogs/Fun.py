ae0b84bc1ce76fc722f47456838cbb7db966f0a5
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-10 16:44:46 +0200

    I dunno what to put here sorry future Matthew

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,4 +1,3 @@
-import concurrent.futures
 import random
 from io import BytesIO
 
@@ -13,21 +12,6 @@ from utils.fun.fortunes import fortunes
 from utils.secret import *
 
 session = aiohttp.ClientSession()
-
-
-class Memes:
-
-    def hitler(self, user):
-        meme = PIL.Image.open(BytesIO(requests.get('https://i.imgflip.com/2kycbm.jpg').content))
-        meme = meme.resize((640, 480), PIL.Image.ANTIALIAS)
-        user = PIL.Image.open(BytesIO(requests.get(user.avatar_url).content))
-        user = user.resize((180, 180), PIL.Image.ANTIALIAS)
-        meme.paste(user, (65, 60))
-
-        arr = BytesIO()
-        meme.save(arr, format="PNG")
-        arr.seek(0)
-        return arr
 
 
 class Fun(commands.Cog):
@@ -159,10 +143,16 @@ class Fun(commands.Cog):
     @commands.is_owner()
     async def hitler(self, ctx, user=None):
         user = user or ctx.author
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            meme = executor.submit(Memes.hitler(user))
-            arr = meme.result()
-            file = discord.File(arr)
+        meme = PIL.Image.open(BytesIO(requests.get('https://i.imgflip.com/2kycbm.jpg').content))
+        meme = meme.resize((640, 480), PIL.Image.ANTIALIAS)
+        user = PIL.Image.open(BytesIO(requests.get(user.avatar_url).content))
+        user = user.resize((180, 180), PIL.Image.ANTIALIAS)
+        meme.paste(user, (65, 60))
+
+        arr = BytesIO()
+        meme.save(arr, format="PNG")
+        arr.seek(0)
+        file = discord.File(arr)
         await ctx.send(file=file)
 
 
