59331e15d968dae1944dd956061904e73619582e
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-05-21 03:22:30 +0200

    Adding support cog

--- cogs/Support.py
+++ cogs/Support.py
@@ -11,7 +11,7 @@ class Support(commands.Cog):
     @commands.group(invoke_without_command=True, name='support', aliases=['chat'])
     async def support_group(self, ctx):
         await self.bot.pg_con.execute("INSERT INTO support (id, user_id, guild_id) "
-                                      "VALUES ($1, $2, $3)", uuid4(), ctx.author.id, ctx.guild.id)
+                                      "VALUES ($1, $2, $3)", str(uuid4()), ctx.author.id, ctx.guild.id)
 
 
 def setup(bot):
